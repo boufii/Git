33caa9a58fdeeb1052c725a4d13de0417cede3a3
yassin <mac@macs-MacBook-Pro-1.local>
2024-03-19 14:06:03 +0000

    First commit

---
+++ story1.swift
@@ -0,0 +1,15 @@
+import Fondation
+
+struct persone{
+    var name : String
+    var Age : Int
+    var Card_ID : String
+}
+
+
+var yassin = persone(name: "Yassin", Age: 22, Card_ID: "ZT28993")
+
+var fatima = persone(name: "Fatima", Age: 22, Card_ID: "ZT268919")
+
+
+print(yassin.name)
